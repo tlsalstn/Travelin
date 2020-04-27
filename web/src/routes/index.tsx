7f1598c792ef6313264ec303377d2ce8ac30ee60
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-04-27 09:04:33 +0900

    [ADD] Route MainPage

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MainPage from './MainPage';
 
 export const Root: React.FC = () => (
     <Router>
         <Switch>
-            
+            <Route exact path="/" component={MainPage} />
         </Switch>
     </Router>
 );
