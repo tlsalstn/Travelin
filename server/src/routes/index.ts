bc2d72c5455229b228aa2c080803285f1b868574
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-06-09 17:26:31 +0900

    [ADD] Place, Share API

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import user from "./user";
+import places from "./places";
+import bulletin from "./bulletin";
+import { checkToken } from "../middlewares/CheckToken";
 
 const router = Router();
 
 router.use('/user', user);
+router.use('/post', bulletin);
+router.use('/places', places);
 
 export default router;
