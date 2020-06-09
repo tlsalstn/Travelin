77fa425feb73416dc49980dcd5e4e9dcf2183940
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-06-09 17:14:04 +0900

    [ADD] Pages
    Routing main page, auth page, map page

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MapPage from './MapPage';
 import MainPage from './MainPage';
+import AuthPage from './AuthPage';
 
 export const Root: React.FC = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={MainPage} />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/map" component={MapPage} />
         </Switch>
     </Router>
 );
