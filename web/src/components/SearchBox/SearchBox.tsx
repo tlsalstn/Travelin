3fef955444115c0d7372ee0226b3ebc52bd20a69
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-06-10 12:33:06 +0900

    [EDIT] Inject search store

--- web/src/components/SearchBox/SearchBox.tsx
+++ web/src/components/SearchBox/SearchBox.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './SearchBox.scss';
 import { observer, inject } from 'mobx-react';
 import Rsltsrch from '../Rsltsrch/Rsltsrch';
-import Info from '../Info/Info';
 
 interface Props {
-    isShow: boolean;
     store?: any;
+    isShow: boolean;
 }
 
 interface State {
@@ -16,31 +15,36 @@ interface State {
 @inject("store")
 @observer
 class SearchBox extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            rsltOrInfo: false
-        };
+    componentDidMount() {
+        // const { setSearchBox } = this.props.store.mapStore;
+        // const container = document.getElementById("searchBox");
+
+        // setSearchBox(container);
     }
-
-
 
     render() {
         const { isShow } = this.props;
-        const { state, handleChangeInput, handleClickDelete, handleClickSearch, onKeyUp } = this.props.store.SearchInputStore;
+        const { value, handleChange, search } = this.props.store.searchStore;
+        const { addMarker } = this.props.store.mapStore;
+
+        const handleKeyUp = (e: any) => {
+            if(e.keyCode === 13) handleClick();
+        }
+
+        const handleClick = async () => {
+            const result = await search();
+            result !== undefined ? addMarker(result.geometry.location.lat, result.geometry.location.lng) : alert("No results");
+        }
 
         return (
             <div className="SearchBox" style={isShow ? {display: "block"} : {display: "none"}}>
                 <div className="SearchBox-Input">
-                    <input type="text" placeholder="검색" onChange={e => handleChangeInput(e)} value={state.value} onKeyUp={onKeyUp} />
-                    <button style={state.isInputed ? {display: "block"} : {display: "none"}} onClick={handleClickDelete}>
-                        <img src="https://img.icons8.com/ios/50/000000/circled-x.png" alt="delect"/>
-                    </button>
-                    <button onClick={handleClickSearch}>
+                    <input id="searchBox" type="text" placeholder="검색" onChange={e => handleChange(e)} value={value} onKeyUp={e => handleKeyUp(e)} />
+                    <button onClick={() => handleClick()}>
                         <img src={"https://img.icons8.com/ios-glyphs/60/000000/search.png"} alt="search" />
                     </button>
                 </div>
-                {this.state.rsltOrInfo ? <Info /> : <Rsltsrch />}
+                {/* <Rsltsrch /> */}
             </div>
         );
     }
