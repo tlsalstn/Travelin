985ca408d20f5d7969e225b62b9627722dae6756
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-06-10 10:45:39 +0900

    [ADD] Search store
    Place search

--- web/src/stores/Search.ts
+++ web/src/stores/Search.ts
@@ -1,101 +1,30 @@
-import { observable, action } from 'mobx';
-import axios from 'axios';
-import { address } from '../config/Adrs.json';
-import { token } from '../config/token.json';
+import { observable, action } from "mobx";
+import { ChangeEvent } from "react";
+import Axios from "axios";
+import { address } from "../config/adrs.json";
 
-interface State {
-    value: string;
-    isInputed: boolean;
-}
+export class SearchStore {
+    @observable value: string = "";
 
-interface Result {
-    documents: any;
-    meta: any;
-}
-
-class SearchInputStore {
-    @observable location: String[] = [];
-
-    @observable state: State = {
-        value: "",
-        isInputed: false
+    @action handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.value = e.currentTarget.value;
     }
 
-    @observable result: Result = {
-        documents: [],
-        meta: []
-    };
-
-    // Input이 비었는지 체크
-    @action handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.state.value = e.target.value;
-
-        if(e.target.value === "") {
-            this.state.isInputed = false;
-        } else {
-            this.state.isInputed = true;
-        }
-    }
-
-    // Input 초기화 버튼
-    @action handleClickDelete = () => {
-        this.state.value = "";
-        this.state.isInputed = false;
-        this.result = {
-            documents: [],
-            meta: []
-        }
-    }
-    
-    @action handleClickSearch = () => {
-        this.searchKeyword();
-    }
-
-    @action onKeyUp = (e: React.KeyboardEvent) => {
-        if(e.keyCode === 13) {
-            this.searchKeyword();
-        }
-    }
-
-    // result의 id 값의 위치 얻기
-    @action getLocation = (id: number) => {
-        console.log(id);
-        this.location = [this.result.documents[id].y, this.result.documents[id].x];
-        return [this.result.documents[id].y, this.result.documents[id].x];
-    }
-
-    // kakao api 키워드 검색
-    searchKeyword = async () => {
-        let size = 15;
-
-        if(this.state.value === "") {
-            this.result = {
-                documents: [],
-                meta: []
-            };
-            return;
-        }
-
+    @action search = async () => {
         try {
-            const result = await axios({
-                url: address.keyWrdSrch,
+            const result = await Axios({
                 method: "GET",
-                headers: {
-                    "Authorization": "KakaoAK " + token.kkorstapk
-                },
+                url: address.localhost + "/places/findPlace",
                 params: {
-                    query: this.state.value,
-                    size: size,
-                    page: 1
+                    input: this.value,
+                    inputtype: "textquery",
+                    fields: "formatted_address,type,geometry,name"
                 }
             });
 
-            this.result = result.data;
-            console.log(result);
-        } catch(error) {
+            return result.data.candidates[0];
+        } catch (error) {
             console.log(error);
         }
     }
 }
-
-export default new SearchInputStore();
--- web/src/stores/index.ts
+++ web/src/stores/index.ts
@@ -1,10 +1,12 @@
 import { MapStore } from './Map';
 import { WeatherStore } from './Weather';
+import { SearchStore } from './Search';
 import { LoginStore, RegisterStore } from "./Auth";
 
 const mapStore = new MapStore();
 const weatherStore = new WeatherStore();
+const searchStore = new SearchStore();
 const loginStore = new LoginStore();
 const registerStore = new RegisterStore();
 
-export default { mapStore, weatherStore, loginStore, registerStore }
+export default { mapStore, weatherStore, searchStore, loginStore, registerStore };
