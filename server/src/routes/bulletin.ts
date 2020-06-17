3782c558c63debf0e6166602e85941f5fe18d00d
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-06-17 11:44:57 +0900

    [EDIT] Add middleware
    Add check token middleware

--- server/src/routes/bulletin.ts
+++ server/src/routes/bulletin.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import ShareController from "../controllers/ShareController";
+import { checkToken } from "../middlewares/CheckToken";
 
 const router = Router();
 
-router.post("/createShare", ShareController.createPost);
+router.post("/createShare", checkToken, ShareController.createPost);
 router.get("/getShare", ShareController.getPosts);
 
 export default router;
