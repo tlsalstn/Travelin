644f7668c7355c6e4c16cb306714809f7bb3c4fd
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-07-06 11:11:42 +0900

    [EDIT] Code cleanup
    Add comment
    Code efficiency

--- web/src/stores/Map.ts
+++ web/src/stores/Map.ts
@@ -15,11 +15,32 @@ interface Map {
     panTo?: any;
 }
 
+interface Center {
+    lat: number;
+    lng: number;
+}
+
+interface Option {
+    zoom: number;
+    center: Center;
+    mapTypeControl: boolean;
+}
+
 interface Waypoints {
     location: string;
 };
 
 export class MapStore {
+    directionsService: any;
+    directionsRenderer: any;
+    marker: any;
+    defaultCenter: Center = { lat: 35.663232, lng: 128.413708 };
+    defaultOptions: Option = {
+        zoom: 10,
+        center: this.defaultCenter,
+        mapTypeControl: false
+    }
+
     @observable map: Map = {};
     @observable points: Waypoints[] = [
         {
@@ -30,83 +51,59 @@ export class MapStore {
         }
     ];
 
+    // 경유지 추가 버튼 클릭 시 points에 추가
     initPoint: Waypoints = {
         location: ""
     }
 
+    // 맵 저장
     setMap = (map: Map) => this.map = map;
-    @action getMap = () => {
-        console.log(this.map);
-        return this.map;
+
+    // 길 생성 설정
+    setDirections = () => {
+        this.directionsService = new window.google.maps.DirectionsService();
+        this.directionsRenderer = new window.google.maps.DirectionsRenderer();
+        this.directionsRenderer.setMap(this.map);
     }
 
-    directionsService: any;
-    directionsRenderer: any;
-    marker: any;
+    createMap = (container: object) => {
+        const map = new window.google.maps.Map(container, this.defaultOptions);
 
-    @action load = async (container: object, travelMode: string, points: Waypoints[]) => {
-        console.log(points);
-        window.onload = () => {
-            const center = { lat: 35.663232, lng: 128.413708 };
-            const option = {
-                zoom: 10,
-                center: center,
-                mapTypeControl: false
-            }
-
-            const map = new window.google.maps.Map(container, option);
-
-            this.setMap(map);
-
-            this.directionsService = new window.google.maps.DirectionsService();
-            this.directionsRenderer = new window.google.maps.DirectionsRenderer();
-            this.directionsRenderer.setMap(this.map);
-
-            this.directionsService.route({
-                origin: points[0].location,
-                destination: points[this.points.length - 1].location,
-                waypoints: points.slice(1, this.points.length - 1).map(item => {
-                    return {
-                        location: item.location
-                    }
-                }),
-                travelMode: travelMode
-            }, (response: object, status: string): void => {
-                console.log(response);
-                if (status === "OK") {
-                    this.directionsRenderer.setDirections(response);
-                } else {
-                    alert("No way found");
-                }
-            });
-        }
+        this.setMap(map);
+        this.setDirections();
     }
 
     // 맵 생성
     @action initMap = (container: object) => {
-        window.onload = () => {
-            const center = { lat: 35.663232, lng: 128.413708 };
-            const option = {
-                zoom: 10,
-                center: center,
-                mapTypeControl: false
+        if(document.readyState === "complete") {
+            this.createMap(container);
+        } else {
+            window.onload = () => {
+                this.createMap(container);
             }
-
-            const map = new window.google.maps.Map(container, option);
-
-            this.setMap(map);
-
-            this.directionsService = new window.google.maps.DirectionsService();
-            this.directionsRenderer = new window.google.maps.DirectionsRenderer();
-            this.directionsRenderer.setMap(this.map);
         }
     }
 
+    // 게시판에서 저장된 맵 불러오기
+    @action load = async (container: object, travelMode: string, points: Waypoints[]) => {
+        if(document.readyState === "complete") {
+            this.createMap(container);
+            this.directions(travelMode, points);
+        } else {
+            window.onload = () => {
+                this.createMap(container);
+                this.directions(travelMode, points);
+            }
+        }
+    }
+
+    // 지도 이동
     @action moveTo = (lat: number, lng: number) => {
         this.map.panTo(new window.google.maps.LatLng(lat, lng));
     }
 
-    @action addMarker = (lat: number, lng: number) => {
+    // 마커 추가
+    @action addMarker = (lat: number, lng: number, title: string = "Marker") => {
         this.moveTo(lat, lng);
 
         if (this.marker) this.marker.setMap(null);
@@ -114,13 +111,12 @@ export class MapStore {
         this.marker = new window.google.maps.Marker({
             position: { lat, lng },
             map: this.map,
-            title: "Marker"
+            title: title
         });
     }
 
     // 길 생성
     @action directions = async (travelMode: string, points: Waypoints[] = this.points) => {
-        // this.directionsRenderer.setMap(this.map);
         this.directionsService.route({
             origin: points[0].location,
             destination: points[this.points.length - 1].location,
@@ -140,11 +136,14 @@ export class MapStore {
         });
     }
 
+    // 경유지 추가
     @action addInput = () => {
         this.points.push(this.initPoint);
     }
 
+    // 경유지 삭제
     @action removeInput = (id: number) => {
+        // 경유지가 2개 이하가 될 경우 값만 삭제
         if (this.points.length <= 2) {
             this.points[id].location = "";
             return;
@@ -152,6 +151,7 @@ export class MapStore {
         this.points.splice(id, 1);
     }
 
+    // 경유지 값 수정
     @action changeInput = (id: number, e: ChangeEvent<HTMLInputElement>) => {
         this.points[id].location = e.target.value;
     }
